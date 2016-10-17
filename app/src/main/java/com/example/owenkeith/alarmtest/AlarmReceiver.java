bc5d971d5155dc554f2379c79b0ffe5d1db72f70
IEatSouls <owencraggskeith@gmail.com>
2016-10-17 12:44:30 -0700

    Update AlarmReceiver.java

--- app/src/main/java/com/example/owenkeith/alarmtest/AlarmReceiver.java
+++ app/src/main/java/com/example/owenkeith/alarmtest/AlarmReceiver.java
@@ -5,31 +5,23 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.NotificationCompat;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    NotificationCompat.Builder mBuilder;
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        mBuilder= new NotificationCompat.Builder(context);
-
         // For our recurring task, we'll just display a message
-        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
-        displayNotification("notify", context);
-
-    }
-    private void displayNotification(String s, Context context) {
-
-
-        Notification notify = mBuilder.build();
-        mBuilder.setSmallIcon(R.drawable.ic_face_black_24dp);
-        mBuilder.setContentTitle("Notification Alert, Click Me!");
-        mBuilder.setContentText("Hi, This is Android Notification Detail!");
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(9999,notify);
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID,0);
+        notificationManager.notify(id, notification);
+        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
+
     }
+
 }
